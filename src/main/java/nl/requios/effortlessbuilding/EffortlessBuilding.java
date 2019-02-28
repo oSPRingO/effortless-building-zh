346fc6d533c362c2825f97586cb0fb267a6e0da8
Christian Knaapen <christianknaapen@hotmail.com>
2019-02-28 18:25:39 +0100

    Fixed mouse clicks in Modifier Settings sometimes being in the wrong place. Fixed typing in multiple text fields at once.

--- src/main/java/nl/requios/effortlessbuilding/EffortlessBuilding.java
+++ src/main/java/nl/requios/effortlessbuilding/EffortlessBuilding.java
@@ -36,7 +36,7 @@ public class EffortlessBuilding
 {
     public static final String MODID = "effortlessbuilding";
     public static final String NAME = "Effortless Building";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     @Mod.Instance(EffortlessBuilding.MODID)
     public static EffortlessBuilding instance;
