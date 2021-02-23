92a1792285ae0c88cb8eac172b649c4997882376
MrPigHead <2519700452@qq.com>
2021-02-23 17:03:06 +0800

    Change name to EffortlessBuildingZh

--- src/main/java/nl/requios/effortlessbuilding/EffortlessBuilding.java
+++ src/main/java/nl/requios/effortlessbuilding/EffortlessBuilding.java
@@ -44,7 +44,7 @@ import org.apache.logging.log4j.Logger;
 @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
 public class EffortlessBuilding
 {
-    public static final String MODID = "effortlessbuilding";
+    public static final String MODID = "effortlessbuildingzh";
     public static final String NAME = "Effortless Building";
     public static final String VERSION = "1.15.2-2.21";
 
